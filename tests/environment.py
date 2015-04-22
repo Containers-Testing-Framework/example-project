888a5c7ae09b0eeba33ed3823863137f1a12e41d
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-04-22 15:21:17 +0200

    Reuse some common steps

--- tests/environment.py
+++ tests/environment.py
@@ -1,45 +1,11 @@
 # -*- coding: UTF-8 -*-
-import subprocess
-import os
-import logging
 import re
-
-
-def run(command, print_output=True):
-    # TODO: Rewrite me with ansible and move me to common_steps
-    try:
-        print("Running '%s'" % command)
-        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-        if print_output:
-            print("Return code: 0, output:\n%s" % output)
-        return output
-    except subprocess.CalledProcessError as e:
-        print("Return code: %d, output:\n%s" % (e.returncode, e.output))
-        raise e
+from steps.common_steps.common_environment import run, docker_cleanup
 
 
 def before_all(context):
     # Save run proc as context.run
     context.run = run
-
-
-# TODO move me to common steps
-def cleanup(context):
-    # Read container cid (if available)
-    if not os.path.exists(context.cid_file):
-        return
-
-    cid = None
-    with open(context.cid_file, "r") as f:
-        cid = f.read().strip()
-
-    try:
-        # Cleanup previous container
-        run("docker stop %s" % cid)
-        run("docker kill %s" % cid)
-        run("docker rm %s" % cid)
-    finally:
-        os.remove(context.cid_file)
 
 
 def before_scenario(context, scenario):
@@ -57,7 +23,7 @@ def before_scenario(context, scenario):
         # Make sure we generate nice name here (for images like openshift/postgresql-92-centos7)
         cid_file_name = re.sub(r'\W+', '', context.image)
         context.cid_file = "/tmp/%s.cid" % cid_file_name
-        cleanup(context)
+        docker_cleanup(context)
     except Exception as e:
         print("before_scenario: exception %s" % str(e))
 
@@ -71,6 +37,6 @@ def after_scenario(context, scenario):
             return
         if scenario.status == 'failed':
             run("docker logs %s" % context.cid)
-        cleanup(context)
+        docker_cleanup(context)
     except Exception as e:
         print("after_scenario: exception %s" % str(e))
